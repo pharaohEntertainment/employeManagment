ba9a9ae6cc0cb4154200fdc67654421922d8cccc
mohamed hussein <mohamed.hussein@stategizeit.us>
2018-11-17 22:10:54 +0200

    seeded maritalStates table

---
+++ EmployeMangment/Migrations/201811172008070_seedMaritalStatesTable.cs
@@ -0,0 +1,24 @@
+﻿namespace EmployeMangment.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedMaritalStatesTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("insert into MaritalStates (name) values (N'انسة')");
+            Sql("insert into MaritalStates (name) values (N'اعزب')");
+            Sql("insert into MaritalStates (name) values (N'متزوج')");
+            Sql("insert into MaritalStates (name) values (N'متزوجة')");
+            Sql("insert into MaritalStates (name) values (N'ارمل')");
+            Sql("insert into MaritalStates (name) values (N'ارملة')");
+            Sql("insert into MaritalStates (name) values (N'مطلق')");
+            Sql("insert into MaritalStates (name) values (N'مطلقة')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
